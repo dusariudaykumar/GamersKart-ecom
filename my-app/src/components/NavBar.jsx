bda12310569f6dea401f14f455ad02e8c267b27a
Uday Kumar <dudaykumargoud@gmail.com>
2022-03-19 21:48:44 +0530

    style : refactor navbar icons

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -4,22 +4,43 @@ const NavBar = () => {
   return (
     <div>
       <nav class="navigation-bar nav-container flex">
-        <a href="#" class="hamberger-icon">
+        <a href="#" class="hamberger-icon hide-content">
           <i class="material-icons-outlined">menu</i>
         </a>
         <h4 class="logo brand-name">
           <a href="#"> ReadyMade-UI </a>
         </h4>
-        <div class="navigation-right-icons">
-          <a href="#" class="nav-wishlist-icon">
-            <i class="material-icons-outlined"> favorite_border </i>
+        <div className="nav-list flex ">
+          <a href="#">
+            <p>HOME</p>
           </a>
-          <a href="#" class="nav-cart-icon">
-            <i class="material-icons-outlined"> shopping_cart </i>
+          <a href="#">
+            <p>PRODUCTS</p>
           </a>
-          <a href="#" class="nav-person-icon">
-            <i class="material-icons"> person </i>
-          </a>
+        </div>
+
+        <div class="navigation-right-icons ">
+          <div class="search-filter">
+            <span class="icon-search">
+              <i class="fas fa-search"></i>
+            </span>
+            <input
+              class="search-space"
+              type="search"
+              placeholder="What are you looking for ?"
+            />
+          </div>
+          <div className="nav-right-icons-container">
+            <a href="#" class="nav-wishlist-icon">
+              <i class="material-icons-outlined"> favorite_border </i>
+            </a>
+            <a href="#" class="nav-cart-icon">
+              <i class="material-icons-outlined"> shopping_cart </i>
+            </a>
+            <a href="#" class="nav-person-icon">
+              <i class="material-icons"> person </i>
+            </a>
+          </div>
         </div>
       </nav>
     </div>
