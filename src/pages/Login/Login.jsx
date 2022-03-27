43576cdf9bf77d63dafc6655cd371ad9d7007cf2
Uday Kumar <dudaykumargoud@gmail.com>
2022-03-27 11:54:11 +0530

    added : login functionality

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Login.css";
 const Login = () => {
+  // initial data of the useState
+  const initialLoginData = {
+    email: "",
+    password: "",
+  };
+  const [loginData, setLoginData] = useState(initialLoginData);
+
+  const loginFormChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevLoginFormData) => ({
+      ...prevLoginFormData,
+      [name]: [value],
+    }));
+  };
+  const { email, password } = loginData; // destructering loginData
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const authResp = await axios.post("/api/auth/login", {
+        email: email,
+        password: password,
+      });
+      console.log(authResp);
+      localStorage.setItem("token", authResp.data.encodedToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const testHandler = (e) => {
+    e.preventDefault();
+    setLoginData((prev) => ({
+      ...prev,
+      email: "adarshbalika@gmail.com",
+      password: "adarshbalika",
+    }));
+  };
+
   return (
     <div className="login-wrapper ">
       <form className="flex-col form-container">
@@ -10,13 +48,19 @@ const Login = () => {
           <input
             className="sample-input-email"
             type="email"
+            name="email"
             placeholder="Enter your Email"
+            value={email}
+            onChange={loginFormChangeHandler}
             required
           />
           <input
             className="sample-input-pwd"
             type="password"
+            name="password"
             placeholder="Password"
+            value={password}
+            onChange={loginFormChangeHandler}
             required
           />
           <div className="flex-space-between">
@@ -28,7 +72,12 @@ const Login = () => {
               <Link to="/">Forgot Password</Link>
             </span>
           </div>
-          <button className="btn btn-solid">Login</button>
+          <button onClick={loginHandler} className="btn btn-solid">
+            Login
+          </button>
+          <button onClick={testHandler} className="btn btn-solid">
+            Test
+          </button>
         </div>
         <Link className="sign-up flex" to="/signup">
           Don't have account ? Sign up!
