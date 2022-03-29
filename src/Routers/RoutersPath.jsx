a76c3ada0887819cf323aa54968c8d334eed2a30
Uday Kumar <dudaykumargoud@gmail.com>
2022-03-29 19:20:23 +0530

    added cart route

--- src/Routers/RoutersPath.jsx
+++ src/Routers/RoutersPath.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { HomePage, Login, Products, SignUp } from "../pages/index";
+import { HomePage, Login, Products, SignUp, Cart } from "../pages/index";
 import Mockman from "mockman-js";
 
 const RoutersPath = () => {
@@ -10,6 +10,7 @@ const RoutersPath = () => {
       <Route path="/products" element={<Products />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/cart" element={<Cart />} />
       <Route path="/mockman" element={<Mockman />} />
     </Routes>
   );
