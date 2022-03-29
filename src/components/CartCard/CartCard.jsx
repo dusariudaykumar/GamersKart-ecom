de6e42b592704c2f01b436eac8be00c484564de3
Uday Kumar <dudaykumargoud@gmail.com>
2022-03-29 19:18:40 +0530

    feat : added cart card component

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,40 +1,37 @@
 import React from "react";
-
+import "./CartCard.css";
 const CartCard = () => {
   return (
-    <div className="cards cart-items-container">
-      <div className="card-outer-container card-cart">
-        <div className="card-inner-container card-cart-inner flex">
-          <div className="card-image cart-card-image">
-            <img className="small-img" src="" alt="ps5-controller" />
+    <div class="cart-outer-container">
+      <div class="cart-inner-container ">
+        <div class="cart-image-container">
+          <img
+            class="cart-img"
+            src="https://gamerskart.netlify.app/Asserts/controllers-img/PS5_DUALSENSE.jpg"
+            alt=""
+          />
+        </div>
+        <div class="cart-info">
+          <p class="cart-card-title">PS5™ Controller</p>
+          <div class="price-details">
+            <span class="current-price">₹5,499</span>
+            <span class="previous-price">₹5,990 </span>
+            <span class="discount-percentage">8% off </span>
           </div>
-          <div className="card-info cart-card-info flex-col">
-            <span className="card-title">PS5™ Controller</span>
-            <div className="price-details">
-              <span className="current-price">₹5,499</span>
-              <span className="previous-price">₹5,990 </span>
-              <span className="discount-percentage">8% off </span>
-            </div>
-            <div className="quantity-wrapper">
-              <button className="btn-inc">+</button>
-              <input
-                className="quantity-display"
-                type="number"
-                min="1"
-                value="1"
-              />
-              <button className="btn-dec">-</button>
-            </div>
-            <div className="card-btns">
-              <button className="card-cart-btn remove-item">
-                <i className="material-icons-outlined"> shopping_cart </i>
-                Remove From cart
-              </button>
-              <button className="card-buy-btn wishlist-btn">
-                <i className="material-icons-outlined"> favorite_border </i>
-                Move To Wishlist
-              </button>
-            </div>
+          <div class="cart-qty-container">
+            <button class="cart-qty-inc">+</button>
+            <div class="qty-display">1</div>
+            <button class="cart-qty-dec">-</button>
+          </div>
+          <div>
+            <button class="cart-remove-item cart-color">
+              <i class="material-icons-outlined"> shopping_cart </i>
+              Remove From cart
+            </button>
+            <button class="cart-wishlist-btn wishlist-color">
+              <i class="material-icons-outlined"> favorite_border </i>
+              Move To Wishlist
+            </button>
           </div>
         </div>
       </div>
