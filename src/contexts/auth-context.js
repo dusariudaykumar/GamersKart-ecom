2c04ee247be982867586c9a14019a98a74c3c65c
Uday Kumar <dudaykumargoud@gmail.com>
2022-03-30 22:33:24 +0530

    removed : unused code

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -1,10 +1,13 @@
 import { createContext, useState, useContext } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const initialAuth = {
   status: false,
   encodedToken: "",
   userData: {},
 };
-const AuthContext = createContext(null);
+
+const AuthContext = createContext(initialAuth);
 const useAuth = () => useContext(AuthContext);
 const AuthProvider = ({ children }) => {
   const [authState, setAuthState] = useState(initialAuth);
