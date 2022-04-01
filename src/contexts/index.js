4b5d4d97f7d46ac7bfc7bbacff74394135b122da
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-01 16:08:18 +0530

    updated : export file in  index file

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 export { AuthProvider } from "./auth-context";
 export { FilterProvider } from "./filter-context";
 export { CartProvider } from "./cart-context";
+export { WishlistProvider } from "./wishlist-context";
