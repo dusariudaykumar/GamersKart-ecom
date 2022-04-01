ebf9670fae196ae1b58fe3f62faa31494b7f559a
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-01 16:07:47 +0530

    updated :routes

--- src/index.js
+++ src/index.js
@@ -3,22 +3,29 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { makeServer } from "./server";
-import { AuthProvider, FilterProvider, CartProvider } from "./contexts/index";
+import {
+  AuthProvider,
+  FilterProvider,
+  CartProvider,
+  WishlistProvider,
+} from "./contexts/index";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <BrowserRouter>
-          <FilterProvider>
-            <App />
-          </FilterProvider>
-        </BrowserRouter>
-      </CartProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <WishlistProvider>
+          <CartProvider>
+            <FilterProvider>
+              <App />
+            </FilterProvider>
+          </CartProvider>
+        </WishlistProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
