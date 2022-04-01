1b1bd0c27076bb142a67838bec36c5d042056a98
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-01 19:31:59 +0530

    feat : added outofstock component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../../contexts/cart-context";
 import { useWishlist } from "../../contexts/wishlist-context";
+import { OutOfStockCard } from "../OutofStockCard/OutOfStockCard";
 
 const ProductCard = ({ product, addToCartHandler, addToWishlistHandler }) => {
   const {
@@ -20,73 +21,80 @@ const ProductCard = ({ product, addToCartHandler, addToWishlistHandler }) => {
   return (
     <div key={_id}>
       <div className="cards ">
-        <div className="card-outer-container">
-          <div
-            className={`card-inner-container ${
-              !inStock && "overlay-container"
-            }`}>
-            <div className="card-image">
-              <img className="small-img" src={coverImg} alt="ps5-controller" />
-              {latest && <span className="card-label">New</span>}
-              {!inStock && (
-                <span className="card-label  out-of-stock-label">
-                  Out Of Stock
-                </span>
-              )}
-            </div>
-            <div className="card-info flex-col">
-              <span className="card-title">{title}</span>
-              <div className="rating-badge flex">
-                <span className="rating-badge-number">
-                  {totalStars}
-                  <i className="fas fa-star gold-color"></i>
-                </span>
-                <span className="rating-badge-number">|</span>
-                <span className="rating-badge-number">5</span>
+        {inStock ? (
+          <div className="card-outer-container">
+            <div
+              className="card-inner-container
+              ">
+              <div className="card-image">
+                <img
+                  className="small-img"
+                  src={coverImg}
+                  alt="ps5-controller"
+                />
+                {latest && <span className="card-label">New</span>}
               </div>
-              <div className="price-details">
-                <span className="current-price">₹{sellingPrice}</span>
-                <span className="previous-price">₹{originalPrice} </span>
-                <span className="discount-percentage">
-                  {discountPercent}% off
-                </span>
-                {wishlistItems.find((e) => e._id === _id) ? (
-                  <button
-                    className="btn-wishlist "
-                    onClick={() => removeFromWishlistHandler(_id)}>
-                    <span className="material-icons-outlined favorite ">
-                      favorite
-                    </span>
-                  </button>
-                ) : (
-                  <button
-                    className="btn-wishlist"
-                    onClick={addToWishlistHandler}>
-                    <i className="fas fa-heart "></i>
-                  </button>
-                )}
-              </div>
-              <div className="card-btns">
-                {cartItems.find((e) => e._id === _id) ? (
-                  <Link to="/cart">
-                    <button className="card-cart-btn">
-                      <i className="material-icons"> shopping_cart_checkout </i>
-                      View Cart
+              <div className="card-info flex-col">
+                <span className="card-title">{title}</span>
+                <div className="rating-badge flex">
+                  <span className="rating-badge-number">
+                    {totalStars}
+                    <i className="fas fa-star gold-color"></i>
+                  </span>
+                  <span className="rating-badge-number">|</span>
+                  <span className="rating-badge-number">5</span>
+                </div>
+                <div className="price-details">
+                  <span className="current-price">₹{sellingPrice}</span>
+                  <span className="previous-price">₹{originalPrice} </span>
+                  <span className="discount-percentage">
+                    {discountPercent}% off
+                  </span>
+                  {wishlistItems.find((e) => e._id === _id) ? (
+                    <button
+                      className="btn-wishlist "
+                      onClick={() => removeFromWishlistHandler(_id)}>
+                      <span className="material-icons-outlined favorite ">
+                        favorite
+                      </span>
                     </button>
-                  </Link>
-                ) : (
-                  <button className="card-cart-btn" onClick={addToCartHandler}>
-                    <i className="material-icons"> shopping_cart </i> Add To
-                    Cart
+                  ) : (
+                    <button
+                      className="btn-wishlist"
+                      onClick={addToWishlistHandler}>
+                      <i className="fas fa-heart "></i>
+                    </button>
+                  )}
+                </div>
+                <div className="card-btns">
+                  {cartItems.find((e) => e._id === _id) ? (
+                    <Link to="/cart">
+                      <button className="card-cart-btn">
+                        <i className="material-icons">
+                          {" "}
+                          shopping_cart_checkout{" "}
+                        </i>
+                        View Cart
+                      </button>
+                    </Link>
+                  ) : (
+                    <button
+                      className="card-cart-btn"
+                      onClick={addToCartHandler}>
+                      <i className="material-icons"> shopping_cart </i> Add To
+                      Cart
+                    </button>
+                  )}
+                  <button className="card-buy-btn">
+                    <i className="material-icons">flash_on </i>Buy Now
                   </button>
-                )}
-                <button className="card-buy-btn">
-                  <i className="material-icons">flash_on </i>Buy Now
-                </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <OutOfStockCard product={product} />
+        )}
       </div>
     </div>
   );
