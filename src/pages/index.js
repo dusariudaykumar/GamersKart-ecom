19fe9eba7b3563a0047f5b112bb3775bba35a2a0
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-01 16:09:40 +0530

    update: export files in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { Products } from "./ProductsListing/Products";
 export { Login } from "./Login/Login";
 export { SignUp } from "./SignUp/SignUp";
 export { Cart } from "./CartPage/Cart";
+export { WishList } from "./WishListPage/WishList";
